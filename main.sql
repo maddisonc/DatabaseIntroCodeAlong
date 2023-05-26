827387630d9c6e943f69d933c15f7bccd7de1c90
maddisonc <902821-maddisonc@users.noreply.replit.com>
2023-05-26 17:08:23 +0000

    Initial commit

---
+++ main.sql
@@ -0,0 +1,20 @@
+CREATE TABLE books
+(
+  title TEXT,
+  author TEXT,
+  num_pages INTEGER,
+  is_good BOOLEAN,
+  publication DATE
+);
+
+INSERT INTO books values
+('The Picture of Dorien Gray', 'Oscar Wilde', 210, true, 1890),
+('The Girl with Dragon Tattoo', 'Steig Larsson', 672, true, 2011),
+('Moby Dick', 'Herman Melville', 378, false, 1851);
+
+.print 'Average number of pages'
+SELECT avg(num_pages) AS avg_pages FROM books;
+
+.print
+.print 'Books I have read'
+SELECT * FROM books 

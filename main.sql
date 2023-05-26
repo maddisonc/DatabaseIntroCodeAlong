ec51e51ccb74cbb157e11a6a0afc292c5d3f63d0
maddisonc <38960421+maddisonc@users.noreply.github.com>
2023-05-26 19:38:48 +0000

    Commit.

--- main.sql
+++ main.sql
@@ -10,7 +10,7 @@ CREATE TABLE books
 INSERT INTO books values
 ('The Picture of Dorien Gray', 'Oscar Wilde', 210, true, 1890),
 ('The Girl with Dragon Tattoo', 'Steig Larsson', 672, true, 2011),
-('Moby Dick', 'Herman Melville ', 378, false, 1851);
+('Moby Dick', 'Herman Melville', 378, false, 1851);
 
 .print 'Average number of pages'
 SELECT avg(num_pages) AS avg_pages FROM books;
